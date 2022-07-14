24dbf733e1778cd90f9d72ecfc7519233823caab
LiviaVilarinho <liviavilarinho@yahoo.com.br>
2022-07-14 18:14:41 -0300

    Initial commit

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercícios Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(2f, 4f);
+    }
+}
